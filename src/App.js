da010a8848f86d45e1146e3e7b47565926a78191
vamsee9 <4n0n5ur7@protonmail.com>
2021-03-24 21:28:08 +0530

    added Registration page
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
 
 function RegistrationForm(props) {
+  
   const [state, setState] = useState({
     email: "",
     password: "",
+    confirmPassword: "",
+    successMessage: null,
   });
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -11,47 +16,53 @@ function RegistrationForm(props) {
       ...prevState,
       [id]: value,
     }));
-    };
-    
-    const sendDetailsToServer = () => {
-      if (state.email.length && state.password.length) {
-        props.showError(null);
-        const payload = {
-          email: state.email,
-          password: state.password,
-        };
-        axios
-          .post(API_BASE_URL + "/user/register", payload)
-          .then(function (response) {
-            if (response.status === 200) {
-              setState((prevState) => ({
-                ...prevState,
-                successMessage:
-                  "Registration successful. Redirecting to home page..",
-              }));
-              redirectToHome();
-              props.showError(null);
-            } else {
-              props.showError("Some error ocurred");
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      } else {
-        props.showError("Please enter valid username and password");
-      }
-    };
-
-    const handleSubmitClick = (e) => {
-      e.preventDefault();
-      if (state.password === state.confirmPassword) {
-        sendDetailsToServer();
-      } else {
-        props.showError("Passwords do not match");
-      }
-    };
-
+  };
+  const sendDetailsToServer = () => {
+    if (state.email.length && state.password.length) {
+      props.showError(null);
+      const payload = {
+        email: state.email,
+        password: state.password,
+      };
+      axios
+        .post("/user/register", payload)
+        .then(function (response) {
+          if (response.status === 200) {
+            setState((prevState) => ({
+              ...prevState,
+              successMessage:
+                "Registration successful. Redirecting to home page..",
+            }));
+            localStorage.setItem(response.data.token);
+            redirectToHome();
+            props.showError(null);
+          } else {
+            props.showError("Some error ocurred");
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      props.showError("Please enter valid username and password");
+    }
+  };
+  const redirectToHome = () => {
+    props.updateTitle("Home");
+    props.history.push("/home");
+  };
+  const redirectToLogin = () => {
+    props.updateTitle("Login");
+    props.history.push("/login");
+  };
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+    if (state.password === state.confirmPassword) {
+      sendDetailsToServer();
+    } else {
+      props.showError("Passwords do not match");
+    }
+  };
   return (
     <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
       <form>
@@ -100,6 +111,21 @@ function RegistrationForm(props) {
           Register
         </button>
       </form>
+      <div
+        className="alert alert-success mt-2"
+        style={{ display: state.successMessage ? "block" : "none" }}
+        role="alert"
+      >
+        {state.successMessage}
+      </div>
+      <div className="mt-2">
+        <span>Already have an account? </span>
+        <span className="loginText" onClick={() => redirectToLogin()}>
+          Login here
+        </span>
+      </div>
     </div>
   );
 }
+
+export default withRouter(RegistrationForm);
